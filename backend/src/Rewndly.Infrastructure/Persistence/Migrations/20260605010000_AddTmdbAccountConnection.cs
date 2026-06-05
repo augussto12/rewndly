81157612993d4edb822c432bf3ea28cb4870ca80
Augusto Alvarez <augustoalvarez10@gmail.com>
2026-06-05 02:04:33 -0300

    Make TMDB account migration discoverable

--- backend/src/Rewndly.Infrastructure/Persistence/Migrations/20260605010000_AddTmdbAccountConnection.cs
+++ backend/src/Rewndly.Infrastructure/Persistence/Migrations/20260605010000_AddTmdbAccountConnection.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +7,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Rewndly.Infrastructure.Persistence.Migrations
 {
     /// <inheritdoc />
+    [DbContext(typeof(AppDbContext))]
+    [Migration("20260605010000_AddTmdbAccountConnection")]
     public partial class AddTmdbAccountConnection : Migration
     {
         /// <inheritdoc />
